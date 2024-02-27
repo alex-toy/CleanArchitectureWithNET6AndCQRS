47f6e3f43f25f788c7ea928518a43245c316eaf5
alex-toy <alexei.80@hotmail.fr>
2024-02-27 20:18:10 +0100

    tests

---
+++ ArchitectureTests/ArchitectureTests.cs
@@ -0,0 +1,94 @@
+﻿using Xunit;
+using NetArchTest.Rules;
+using System.Reflection;
+
+namespace ArchitectureTests
+{
+    public class ArchitectureTests
+    {
+        private const string DomainNamespace = "Domain";
+        private const string ApplicationNamespace = "Application";
+        private const string InfrastructureNamespace = "Infrastructure";
+        private const string PresentationNamespace = "Presentation";
+        private const string WebNamespace = "Web";
+
+        [Fact]
+        public void Domain_should_not_have_dependencies_on_other_projects()
+        {
+            Assembly domainAssembly = typeof(Domain.AssemblyReference).Assembly;
+
+            string[] otherProjects = new[]
+            {
+                ApplicationNamespace,
+                InfrastructureNamespace,
+                PresentationNamespace,
+                WebNamespace,
+            };
+
+            TestResult result = Types.InAssembly(domainAssembly).ShouldNot().HaveDependencyOnAll(otherProjects).GetResult();
+
+            Assert.True(result.IsSuccessful);
+            //result.IsSuccessful
+        }
+
+        [Fact]
+        public void Application_should_not_have_dependencies_on_other_projects()
+        {
+            Assembly assembly = typeof(Application.AssemblyReference).Assembly;
+
+            string[] otherProjects = new[]
+            {
+                InfrastructureNamespace,
+                PresentationNamespace,
+                WebNamespace,
+            };
+
+            TestResult result = Types.InAssembly(assembly).ShouldNot().HaveDependencyOnAll(otherProjects).GetResult();
+
+            Assert.True(result.IsSuccessful);
+        }
+
+        [Fact]
+        public void Infrastructure_should_not_have_dependencies_on_other_projects()
+        {
+            Assembly assembly = typeof(Infrastructure.AssemblyReference).Assembly;
+
+            string[] otherProjects = new[]
+            {
+                PresentationNamespace,
+                WebNamespace,
+            };
+
+            TestResult result = Types.InAssembly(assembly).ShouldNot().HaveDependencyOnAll(otherProjects).GetResult();
+
+            Assert.True(result.IsSuccessful);
+        }
+
+        [Fact]
+        public void Presentation_should_not_have_dependencies_on_other_projects()
+        {
+            Assembly assembly = typeof(Presentation.AssemblyReference).Assembly;
+
+            string[] otherProjects = new[]
+            {
+                InfrastructureNamespace,
+                WebNamespace,
+            };
+
+            TestResult result = Types.InAssembly(assembly).ShouldNot().HaveDependencyOnAll(otherProjects).GetResult();
+
+            Assert.True(result.IsSuccessful);
+        }
+
+        [Fact]
+        public void Handlers_should_have_dependency_on_Domain()
+        {
+            Assembly assembly = typeof(Application.AssemblyReference).Assembly;
+
+            PredicateList handlers = Types.InAssembly(assembly).That().HaveNameEndingWith("Handler");
+            TestResult result = handlers.Should().HaveDependencyOnAll(DomainNamespace).GetResult();
+
+            Assert.True(result.IsSuccessful);
+        }
+    }
+}
--- src/Application/Webinars/Queries/GetWebinarById/GetWebinarQueryHandler.cs
+++ src/Application/Webinars/Queries/GetWebinarById/GetWebinarQueryHandler.cs
@@ -3,7 +3,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Application.Abstractions.Messaging;
 using Dapper;
-using Domain.Exceptions;
+//using Domain.Exceptions;
 
 namespace Application.Webinars.Queries.GetWebinarById;
 
@@ -13,20 +13,13 @@ internal sealed class GetWebinarQueryHandler : IQueryHandler<GetWebinarByIdQuery
 
     public GetWebinarQueryHandler(IDbConnection dbConnection) => _dbConnection = dbConnection;
 
-    public async Task<WebinarResponse> Handle(
-        GetWebinarByIdQuery request,
-        CancellationToken cancellationToken)
+    public async Task<WebinarResponse> Handle(GetWebinarByIdQuery request, CancellationToken cancellationToken)
     {
         const string sql = @"SELECT * FROM ""Webinars"" WHERE ""Id"" = @WebinarId";
 
-        var webinar = await _dbConnection.QueryFirstOrDefaultAsync<WebinarResponse>(
-            sql,
-            new { request.WebinarId });
+        var webinar = await _dbConnection.QueryFirstOrDefaultAsync<WebinarResponse>(sql, new { request.WebinarId });
 
-        if (webinar is null)
-        {
-            throw new WebinarNotFoundException(request.WebinarId);
-        }
+        //if (webinar is null) throw new WebinarNotFoundException(request.WebinarId);
 
         return webinar;
     }
