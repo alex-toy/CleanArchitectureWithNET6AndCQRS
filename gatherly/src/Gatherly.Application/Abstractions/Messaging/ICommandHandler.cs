5000f3cec908bf488d2d24a6a8d4ffc5600c11b7
alex-toy <alexei.80@hotmail.fr>
2024-04-20 07:31:42 +0200

    refacto

--- gatherly/src/Gatherly.Application/Abstractions/Messaging/ICommandHandler.cs
+++ gatherly/src/Gatherly.Application/Abstractions/Messaging/ICommandHandler.cs
@@ -3,13 +3,10 @@ using MediatR;
 
 namespace Gatherly.Application.Abstractions.Messaging;
 
-public interface ICommandHandler<TCommand> : IRequestHandler<TCommand, Result>
-    where TCommand : ICommand
+public interface ICommandHandler<TCommand> : IRequestHandler<TCommand, Result> where TCommand : ICommand
 {
 }
 
-public interface ICommandHandler<TCommand, TResponse>
-    : IRequestHandler<TCommand, Result<TResponse>>
-    where TCommand : ICommand<TResponse>
+public interface ICommandHandler<TCommand, TResponse> : IRequestHandler<TCommand, Result<TResponse>> where TCommand : ICommand<TResponse>
 {
 }
